12370738392f53ae5e30675d653878a17c8b3fdd
oshiro <tsubasa.oshiro@gmail.com>
2018-08-21 17:50:25 +0000

    Validation User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+  validates :name, presence: true, length: { maximum: 150 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255 }, 
+                    uniqueness: { case_sensitive: false }, 
+                    format: { with: VALID_EMAIL_REGEX }
+                    
+
+  def downcase_email
+    email.downcase!
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name:"Hogehoge Fugao", email:"fugao@hogeo.com")
+  end
+  
+  test "should be valid" do 
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+  
+  test "name should not be too long" do
+    @user.name = 'a' * 151
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+  
+  test "email should be not too long" do
+    @user.email = 'a' * (256 - "@example.com".length)
+    assert_not @user.valid?
+  end
+  
+  test "email should be written in a right format" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address} should be valid"
+    end
+  end
+  
+  test "email should not be written in a wrong format" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
+                        foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
+  
+  test "email should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAmPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
