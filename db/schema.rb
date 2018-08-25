27ac7d099d35ee9ef97523c0ed07b98befe91a77
oshiro <tsubasa.oshiro@gmail.com>
2018-08-25 17:43:20 +0000

    Add Song model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_151842) do
+ActiveRecord::Schema.define(version: 2018_08_25_173909) do
+
+  create_table "songs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
