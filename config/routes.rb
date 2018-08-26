23028ab14483606bcfdc59f9ce8e05fc84727630
oshiro <tsubasa.oshiro@gmail.com>
2018-08-26 15:32:13 +0000

    静的なページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'users#index'
+
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  # root 'users#index'
   resources :users
 end
